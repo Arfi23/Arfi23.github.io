8a27a6eaaa26ab65e2a72d293f58b906b3292e3c
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-15 10:15:43 +0700

    Membuat fitur drag pada square di dalam grid

--- app.js
+++ app.js
@@ -25,4 +25,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   createBoard();
+
+  // Saat melakukan drag
+  squares.forEach((square) => square.addEventListener("dragstart", dragStart));
+  squares.forEach((square) => square.addEventListener("dragend", dragEnd));
+  squares.forEach((square) => square.addEventListener("dragover", dragOver));
+  squares.forEach((square) => square.addEventListener("dragenter", dragEnter));
+  squares.forEach((square) => square.addEventListener("dragleave", dragLeave));
+  squares.forEach((square) => square.addEventListener("drop", dragDrop));
+
+  function dragStart() {
+    console.log(this.id, "dragstart");
+  }
+
+  function dragOver() {
+    console.log(this.id, "dragover");
+  }
+
+  function dragEnter() {
+    console.log(this.id, "dragenter");
+  }
+
+  function dragLeave() {
+    console.log(this.id, "dragleave");
+  }
+
+  function dragEnd() {
+    console.log(this.id, "dragend");
+  }
+
+  function dragDrop() {
+    console.log(this.id, "dragdrop");
+  }
 });
