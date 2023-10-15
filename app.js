2333f0822d03cffba85e83f1831d80adbbd22ca3
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-15 10:01:21 +0700

    Membuat papan grid dengan berisikan kotak kecil berwarna

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const width = 8;
   const squares = [];
 
+  const proLangs = [
+    "red", // Ruby
+    "yellow", // Javascript
+    "orange", // Swift
+    "purple", // C-Sharp
+    "green", // python
+    "blue", // C++
+  ];
+
   // Membuat papan
   function createBoard() {
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement("div");
+      square.setAttribute("draggable", true);
+      square.setAttribute("id", i);
+      let randomColor = Math.floor(Math.random() * proLangs.length);
+      square.style.backgroundColor = proLangs[randomColor];
       grid.appendChild(square);
       squares.push(square);
     }
