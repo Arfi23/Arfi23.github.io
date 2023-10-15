f261c6cd50a1d6aaf2dbeb06293fa5c1a2466db2
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-15 10:37:19 +0700

    Membuat fitur tukar square dengan mengikuti aturan gerakan yang valid

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ document.addEventListener("DOMContentLoaded", () => {
   createBoard();
 
   // Saat melakukan drag
+  let colorBeingDragged;
+  let colorBeingReplaced;
+  let squareIdBeingDragged;
+  let squareIdBeingReplaced;
+
   squares.forEach((square) => square.addEventListener("dragstart", dragStart));
   squares.forEach((square) => square.addEventListener("dragend", dragEnd));
   squares.forEach((square) => square.addEventListener("dragover", dragOver));
@@ -35,14 +40,19 @@ document.addEventListener("DOMContentLoaded", () => {
   squares.forEach((square) => square.addEventListener("drop", dragDrop));
 
   function dragStart() {
+    colorBeingDragged = this.style.backgroundColor;
+    squareIdBeingDragged = parseInt(this.id);
+    // console.log(colorBeingDragged);
     console.log(this.id, "dragstart");
   }
 
-  function dragOver() {
+  function dragOver(e) {
+    e.preventDefault();
     console.log(this.id, "dragover");
   }
 
-  function dragEnter() {
+  function dragEnter(e) {
+    e.preventDefault();
     console.log(this.id, "dragenter");
   }
 
@@ -50,11 +60,28 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(this.id, "dragleave");
   }
 
-  function dragEnd() {
-    console.log(this.id, "dragend");
-  }
-
   function dragDrop() {
     console.log(this.id, "dragdrop");
+    colorBeingReplaced = this.style.backgroundColor;
+    squareIdBeingReplaced = parseInt(this.id);
+    this.style.backgroundColor = colorBeingDragged;
+    squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced;
+  }
+
+  function dragEnd() {
+    console.log(this.id, "dragend");
+    // gerakan yang valid
+    let validMoves = [squareIdBeingDragged - 1, squareIdBeingDragged - width, squareIdBeingDragged + 1, squareIdBeingDragged + width];
+
+    let validMove = validMoves.includes(squareIdBeingReplaced);
+
+    if (squareIdBeingReplaced && validMove) {
+      squareIdBeingReplaced = null;
+    } else if (squareIdBeingReplaced && !validMove) {
+      squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced;
+      squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged;
+    } else {
+      squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged;
+    }
   }
 });
